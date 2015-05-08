2b63ff2cf2102783a429633452e99a88531e417c
Lee Sharma <lee@leesharma.com>
2015-05-08 16:47:45 -0400

    add content to new
    -add: form on notes/new.html
    -add: value to submit buttons
    -add: link to new from index
    -add: new controller mirrors edit controller

--- app/assets/javascript/controllers/noteCreateController.js
+++ app/assets/javascript/controllers/noteCreateController.js
@@ -1,3 +1,14 @@
-angular.module('NoteWrangler').controller('NotesCreateController', function() {
+angular.module('NoteWrangler').controller('NotesCreateController', function(Note, $scope, $location) {
+  $scope.note = new Note();
+  $scope.isSubmitting = false;
 
+  $scope.saveNote = function(note) {
+    $scope.isSubmitting = true;
+    note.$save()
+      .then(function(){
+        $location.path('/notes/' + note.id);
+      }).finally(function(){
+        $scope.isSubmitting = false;
+      });
+  };
 });
