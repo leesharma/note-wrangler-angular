3dd9a27e6c89054e1c9663d73b99ee04b3ff8181
Lee Sharma <lee@leesharma.com>
2015-05-08 14:58:10 -0400

    add content to edit
    -add: set $scope.note in controller
    -add: saveNote() saves note and redirects
    -add: content to edit.html

--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -1,3 +1,12 @@
-angular.module('NoteWrangler').controller('NotesEditController', function() {
+angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams, $location) {
+  $scope.note = Note.get({id: $routeParams.id});
+  $scope.isSubmitting = false;
 
+  $scope.saveNote = function(note) {
+    $scope.isSubmitting = true;
+    note.$update().finally(function(){
+      $scope.isSubmitting = false;
+      $location.path('/notes/' + note.id);
+    });
+  };
 });
--- app/assets/javascript/services/note.js
+++ app/assets/javascript/services/note.js
@@ -1,3 +1,7 @@
 angular.module('NoteWrangler').factory('Note', function ($resource) {
-  return $resource('/notes/:id');
+  return $resource('/notes/:id',  {id: "@id"}, {
+    update: {
+      method: "PUT"
+    }
+  });
 });
