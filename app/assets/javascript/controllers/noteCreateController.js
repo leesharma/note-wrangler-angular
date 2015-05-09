61a904607f0636b7bd9c7c3c152ee9818d04e8ba
Lee Sharma <lee@leesharma.com>
2015-05-08 20:48:50 -0400

    add category and author selects to note forms
    -add: category service
    -add: users and categories to note create and index controllers
    -add: ng-select select fields to forms

--- app/assets/javascript/controllers/noteCreateController.js
+++ app/assets/javascript/controllers/noteCreateController.js
@@ -1,6 +1,8 @@
-angular.module('NoteWrangler').controller('NotesCreateController', function(Note, $scope, $location) {
+angular.module('NoteWrangler').controller('NotesCreateController', function(Note, Category, User, $scope, $location) {
   $scope.note = new Note();
   $scope.isSubmitting = false;
+  $scope.categories = Category.query();
+  $scope.users = User.query();
 
   $scope.saveNote = function(note) {
     $scope.isSubmitting = true;
--- app/assets/javascript/controllers/noteEditController.js
+++ app/assets/javascript/controllers/noteEditController.js
@@ -1,6 +1,8 @@
-angular.module('NoteWrangler').controller('NotesEditController', function(Note, $scope, $routeParams, $location) {
+angular.module('NoteWrangler').controller('NotesEditController', function(Note, Category, User, $scope, $routeParams, $location) {
   $scope.note = Note.get({id: $routeParams.id});
   $scope.isSubmitting = false;
+  $scope.categories = Category.query();
+  $scope.users = User.query();
 
   $scope.saveNote = function(note) {
     $scope.isSubmitting = true;
