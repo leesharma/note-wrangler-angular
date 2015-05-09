9d1cd204c333396ddcbd5eb3aa37ece36a003c07
Lee Sharma <lee@leesharma.com>
2015-05-09 02:12:29 -0400

    remove hardcoded port from app.js
    -fix: app.listen(8000 to app.listen(process.env.PORT || 8000
    
    for Heroku due to changing ports

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require("./server/routes");
 
 // Start the server
-var server = app.listen(8000, function() {
+var server = app.listen(process.env.PORT || 8000, function() {
  console.log('Listening on port %d', server.address().port);
 });
